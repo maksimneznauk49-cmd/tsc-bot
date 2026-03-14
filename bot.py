4a5689d41dc06ea9656bcaf694fdbd4df1a270da
maksimneznauk49-cmd <maksimneznauk49@gmail.com>
2026-03-14 16:41:47 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import requests
-import time
 
 TOKEN = "8632657146:AAH1eIOFEEk7XLctRU_H7mJ3Of2exLoM_Jg"
 CHAT_ID = "5198714684"
@@ -12,12 +11,9 @@ def send(msg):
 
 URL = "https://api.hsc.gov.ua/api/queue"  # посилання на перевірку ТСЦ
 
-while True:
-    try:
-        r = requests.get(URL)
-        if "free" in r.text.lower():
-            send("🔥 Зʼявився вільний запис у ТСЦ 6143!")
-    except Exception as e:
-        print(e)
-    
-    time.sleep(300)  # перевірка кожні 5 хвилин
+try:
+    r = requests.get(URL)
+    if "free" in r.text.lower():
+        send("🔥 Зʼявився вільний запис у ТСЦ 6143!")
+except Exception as e:
+    print(e)
