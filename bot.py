69041a38fcc4cbc512a2ec2375884f670dd4b3a3
maksimneznauk49-cmd <maksimneznauk49@gmail.com>
2026-03-14 15:13:18 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import requests
+import time
+
+TOKEN = "8632657146:AAH1eIOFEEk7XLctRU_H7mJ3Of2exLoM_Jg"
+CHAT_ID = "5198714684"
+
+def send(msg):
+    requests.get(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        params={"chat_id": CHAT_ID, "text": msg}
+    )
+
+URL = "https://api.hsc.gov.ua/api/queue"  # посилання на перевірку ТСЦ
+
+while True:
+    try:
+        r = requests.get(URL)
+        if "free" in r.text.lower():
+            send("🔥 Зʼявився вільний запис у ТСЦ 6143!")
+    except Exception as e:
+        print(e)
+    
+    time.sleep(300)  # перевірка кожні 5 хвилин
